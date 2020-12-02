6050cd43c131079dd7fd73228a428483c7316063
dchaika <dchaika1990@gmail.com>
2020-12-02 12:12:57 +0200

    drop

---
+++ src/js/modules/drop.js
@@ -0,0 +1,48 @@
+const drop = () => {
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false)
+        })
+    })
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highLight(item) {
+        item.closest('.file_upload').style.border = '5px solid yellow';
+        item.closest('.file_upload').style.backgroundColor = 'rgba(0,0,0, .7)';
+    }
+
+    function unHighLight(item) {
+        item.closest('.file_upload').style.border = 'none';
+        item.closest('.file_upload').style.backgroundColor = 'inherit';
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => highLight(input), false)
+        })
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => unHighLight(input), false)
+        })
+    });
+
+    fileInputs.forEach(input => {
+        input.addEventListener('drop', (e) => {
+            input.files = e.dataTransfer.files;
+            let arr = input.files[0].name.split('.');
+            let dots = arr[0].length > 6 ? '...' : '.'
+            let name = arr[0].substring(0, 6) + dots + arr[1];
+            input.previousElementSibling.textContent = name;
+        })
+    })
+}
+
+export default drop;
